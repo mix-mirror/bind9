eacdb74952bee08659258751fabe0b1224f737f0
Mark Andrews <marka@isc.org>
2021-06-26 12:45:53 +1000

    Add ZONEMD checking options
    Add options (not yet functional) to named.conf and the
    named-checkzone command line to enable ZONEMD checks.
    Update documentation to describe them.

--- bin/check/check-tool.h
+++ bin/check/check-tool.h
@@ -24,6 +24,7 @@
 #include <dns/masterdump.h>
 #include <dns/types.h>
 #include <dns/zone.h>
+#include <dns/zonemd.h>
 
 isc_result_t
 setup_logging(FILE *errout);
@@ -46,3 +47,4 @@ extern bool docheckns;
 extern bool dochecksrv;
 extern bool docheckrpz;
 extern dns_zoneopt_t zone_options;
+extern dns_zoneopt_t zonemd_options;
