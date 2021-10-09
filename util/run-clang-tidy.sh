f249a4ff6e503d61bb9045663f9f80f6dd157668
Ondřej Surý <ondrej@sury.org>
2021-10-09 10:19:17 +0200

    Improve the clang-format and clang-tidy integration
    Add util/run-clang-tidy.sh script that can be used to run clang-tidy
    on the compilation database.  The compilation database can be created
    by running:
    
        pip3 install compiledb
        autoreconf -fi
        ./configure --enable-developer
        make bind.keys.h
        (cd lib/dns && \
         make gen include/dns/enumtype.h \
                  include/dns/enumclass.h \
                  include/dns/rdatastruct.h \
                  code.h)
        (make -nwk all || true) | compiledb

---
+++ util/run-clang-tidy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+CLANG_TIDY_CHECKS="-*"
+CLANG_TIDY_CHECKS="$CLANG_TIDY_CHECKS,readability-braces-around-statements"
+CLANG_TIDY_CHECKS="$CLANG_TIDY_CHECKS${*:+,}${*}"
+
+python3 ./util/run-clang-tidy \
+	-clang-tidy-binary "${CLANG_TIDY:-clang-tidy-19}" \
+	-clang-apply-replacements-binary "${CLANG_APPLY_REPLACEMENTS:-clang-apply-replacements-19}" \
+	-checks="$CLANG_TIDY_CHECKS" \
+	-j 9 \
+	-fix \
+	-quiet
