1ef758259f5e8bc4437490a203a59ff4904ecac4
Mark Andrews <marka@isc.org>
2024-01-15 11:47:13 +1100

    Only copy the name data after we know its actual length
    This prevents TSAN errors with the ncache code where the trust byte
    access needs to be protected by a lock.  The old code copied the
    entire region before determining where the name ended.  We now
    determine where the name ends then copy just that data and in doing
    so avoid reading the trust byte.
    
    (cherry picked from commit 4ef755ffb0555705e59452a9fb6c9ccecd039d12)

--- lib/dns/name.c
+++ lib/dns/name.c
@@ -996,7 +996,7 @@ dns_name_fromregion(dns_name_t *name, const isc_region_t *r) {
 	unsigned char *offsets;
 	dns_offsets_t odata;
 	unsigned int len;
-	isc_region_t r2;
+	isc_region_t r2 = { .base = NULL, .length = 0 };
 
 	/*
 	 * Make 'name' refer to region 'r'.
@@ -1008,6 +1008,7 @@ dns_name_fromregion(dns_name_t *name, const isc_region_t *r) {
 
 	INIT_OFFSETS(name, offsets, odata);
 
+	name->ndata = r->base;
 	if (name->buffer != NULL) {
 		isc_buffer_clear(name->buffer);
 		isc_buffer_availableregion(name->buffer, &r2);
@@ -1015,13 +1016,8 @@ dns_name_fromregion(dns_name_t *name, const isc_region_t *r) {
 		if (len > DNS_NAME_MAXWIRE) {
 			len = DNS_NAME_MAXWIRE;
 		}
-		if (len != 0) {
-			memmove(r2.base, r->base, len);
-		}
-		name->ndata = r2.base;
 		name->length = len;
 	} else {
-		name->ndata = r->base;
 		name->length = (r->length <= DNS_NAME_MAXWIRE)
 				       ? r->length
 				       : DNS_NAME_MAXWIRE;
@@ -1035,6 +1031,15 @@ dns_name_fromregion(dns_name_t *name, const isc_region_t *r) {
 	}
 
 	if (name->buffer != NULL) {
+		/*
+		 * name->length has been updated by set_offsets to the actual
+		 * length of the name data so we can now copy the actual name
+		 * data and not anything after it.
+		 */
+		if (name->length > 0) {
+			memmove(r2.base, r->base, name->length);
+		}
+		name->ndata = r2.base;
 		isc_buffer_add(name->buffer, name->length);
 	}
 }
