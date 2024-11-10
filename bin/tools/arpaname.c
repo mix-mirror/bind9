ba90187d156a8195f36792feaa7b77ce432fdb92
Petr Menšík <pemensik@redhat.com>
2024-11-10 09:28:59 +0100

    Implement basic phone number ENUM support
    Prepare RFC 6116 enum domain name from a telephone number. Number has to
    start with + to be recognized.

--- bin/tools/arpaname.c
+++ bin/tools/arpaname.c
@@ -18,6 +18,24 @@
 
 #define UNUSED(x) (void)(x)
 
+/* https://www.rfc-editor.org/rfc/rfc6116.html */
+static bool
+is_enum(const char *str) {
+	const char *i = str;
+	unsigned char c;
+	bool valid = false;
+
+	if (*i != '+')
+		return false;
+	for (i = str + 1; *i != '\0'; i++) {
+		c = *i;
+		if (!((c >= '0' && c <= '9') || c == '-'))
+			return false;
+		valid = true;
+	}
+	return valid;
+}
+
 int
 main(int argc, char *argv[]) {
 	unsigned char buf[16];
@@ -38,6 +56,17 @@ main(int argc, char *argv[]) {
 		if (inet_pton(AF_INET, argv[1], buf) == 1) {
 			fprintf(stdout, "%u.%u.%u.%u.IN-ADDR.ARPA\n", buf[3],
 				buf[2], buf[1], buf[0]);
+			argv++;
+			continue;
+		}
+		if (is_enum(argv[1])) {
+			size_t len = strlen(argv[1]);
+			for (i = len - 1; i >= 0; i--) {
+				unsigned char c = argv[1][i];
+				if (c >= '0' && c <= '9')
+					fprintf(stdout, "%c.", c);
+			}
+			fprintf(stdout, "E164.ARPA\n");
 			argv++;
 			continue;
 		}
