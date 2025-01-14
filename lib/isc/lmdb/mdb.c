25b07e2190f1b0a70198a425e7dd27b7b86b2f82
Colin Vidal <colin@isc.org>
2025-01-14 14:02:27 +0100

    Revert "Various changes to integrate LMDB source files"
    This reverts commit 0b5611bda770ed301450d75e85a6390658ffc68b.

--- lib/isc/lmdb/mdb.c
+++ lib/isc/lmdb/mdb.c
@@ -1,16 +1,3 @@
-/*
- * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
- *
- * SPDX-License-Identifier: MPL-2.0
- *
- * This Source Code Form is subject to the terms of the OpenLDAP
- * Public License. If a copy of the MPL was not distributed with this
- * file, you can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * See license comment below for additional information regarding
- * copyright ownership.
- */
-
 /** @file mdb.c
  *	@brief Lightning memory-mapped database library
  *
