426baf2cd7fe2fed11303ebd7c49dd3f67cc9ca8
Ondřej Surý <ondrej@isc.org>
2025-02-12 17:28:49 +0100

    Remove DNS_DBADD_FORCE usage in qpzone.c
    The subtractrdataset() in qpzone.c was adding DNS_DBADD_FORCE flag to
    the add() function, but since the zone database has no concept of
    "trust", it was just confusing no-op flag.  Remove it.

--- lib/dns/qpzone.c
+++ lib/dns/qpzone.c
@@ -4994,8 +4994,8 @@ qpzone_deleterdataset(dns_db_t *db, dns_dbnode_t *dbnode,
 
 	nlock = &qpdb->buckets[node->locknum].lock;
 	NODE_WRLOCK(nlock, &nlocktype);
-	result = add(qpdb, node, nodename, version, newheader, DNS_DBADD_FORCE,
-		     false, NULL, 0 DNS__DB_FLARG_PASS);
+	result = add(qpdb, node, nodename, version, newheader, 0, false, NULL,
+		     0 DNS__DB_FLARG_PASS);
 	NODE_UNLOCK(nlock, &nlocktype);
 	return result;
 }
