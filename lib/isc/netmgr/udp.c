be7e4ca00b0c6a1bdc7866a3a0ad454e76d104a6
Artem Boldariev <artem@boldariev.com>
2025-10-26 11:21:20 +0200

    Suppress UDP retry error messages
    The UDP transport code would produce an error message when
    encountering UV_EGAIN error when sending data.
    
    That error is a recoverable, soft error that signals that the send
    request should be reattempted later and is supposed to be handled by
    the higher level code logic. Thus, the code suppresses producing a
    largely useless error message in this case.

--- lib/isc/netmgr/udp.c
+++ lib/isc/netmgr/udp.c
@@ -710,7 +710,8 @@ isc__nm_udp_send(isc_nmhandle_t *handle, const isc_region_t *region,
 		 */
 		r = uv_udp_try_send(&sock->uv_handle.udp, &uvreq->uvbuf, 1, sa);
 		if (r < 0) {
-			if (can_log_udp_sends()) {
+			/* Do not log retry errors */
+			if (can_log_udp_sends() && r != UV_EAGAIN) {
 				isc__netmgr_log(
 					ISC_LOG_ERROR,
 					"Sending UDP messages failed: %s",
@@ -885,7 +886,7 @@ isc__nm_udp_failed_read_cb(isc_nmsocket_t *sock, isc_result_t result,
 	 * - we don't destroy it (only stoplistening could do that)
 	 */
 
-	if (sock->client) {
+	if (sock->client && !sock->client_udp_read_non_stop) {
 		isc__nmsocket_timer_stop(sock);
 		isc__nm_stop_reading(sock);
 	}
@@ -900,7 +901,7 @@ isc__nm_udp_failed_read_cb(isc_nmsocket_t *sock, isc_result_t result,
 		}
 	}
 
-	if (sock->client) {
+	if (sock->client && !sock->client_udp_read_non_stop) {
 		isc__nmsocket_clearcb(sock);
 		isc__nmsocket_prep_destroy(sock);
 		return;
