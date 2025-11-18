a4243fc1ab0111da84c60a42ee7ab3f8985a5604
Matthijs Mekking <matthijs@isc.org>
2025-11-18 09:56:34 +0100

    Add type parameter to dns_notify_create()
    With Generalized DNS Notifications, a zone may need to send different
    type of NOTIFY messages for different reasons. When creating a new
    notify, allow for specifying the type.

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -12727,8 +12727,9 @@ zone_notify(dns_zone_t *zone, isc_time_t *now) {
 			goto next;
 		}
 
-		if (dns_notify_isqueued(&zone->notifysoa, zone->view->dstport,
-					flags, NULL, &dst, key, transport))
+		if (dns_notify_isqueued(&zone->notifysoa, dns_rdatatype_soa,
+					zone->view->dstport, flags, NULL, &dst,
+					key, transport))
 		{
 			if (key != NULL) {
 				dns_tsigkey_detach(&key);
@@ -12739,8 +12740,8 @@ zone_notify(dns_zone_t *zone, isc_time_t *now) {
 			goto next;
 		}
 
-		dns_notify_create(zone->mctx, zone->view->dstport, flags,
-				  &notify);
+		dns_notify_create(zone->mctx, dns_rdatatype_soa,
+				  zone->view->dstport, flags, &notify);
 		zone_iattach(zone, &notify->zone);
 		notify->src = src;
 		notify->dst = dst;
@@ -12817,15 +12818,15 @@ zone_notify(dns_zone_t *zone, isc_time_t *now) {
 		}
 
 		LOCK_ZONE(zone);
-		isqueued = dns_notify_isqueued(&zone->notifysoa,
-					       zone->view->dstport, flags,
-					       &ns.name, NULL, NULL, NULL);
+		isqueued = dns_notify_isqueued(
+			&zone->notifysoa, dns_rdatatype_soa,
+			zone->view->dstport, flags, &ns.name, NULL, NULL, NULL);
 		UNLOCK_ZONE(zone);
 		if (isqueued) {
 			continue;
 		}
-		dns_notify_create(zone->mctx, zone->view->dstport, flags,
-				  &notify);
+		dns_notify_create(zone->mctx, dns_rdatatype_soa,
+				  zone->view->dstport, flags, &notify);
 		dns_zone_iattach(zone, &notify->zone);
 		dns_name_dup(&ns.name, zone->mctx, &notify->ns);
 		LOCK_ZONE(zone);
