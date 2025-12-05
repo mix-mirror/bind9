c1472def91680db7d8126d02e94e252442fc3763
Ondřej Surý <ondrej@isc.org>
2025-12-05 09:52:56 +0100

    Clean up setresign
    Refactor dns_rdataset_minresign to use the DNS_RDATASET_FOREACH macro.

--- lib/dns/rdataset.c
+++ lib/dns/rdataset.c
@@ -601,38 +601,27 @@ dns_rdataset_getheader(const dns_rdataset_t *rdataset) {
 
 isc_stdtime_t
 dns_rdataset_minresign(dns_rdataset_t *rdataset) {
-	dns_rdata_t rdata = DNS_RDATA_INIT;
-	dns_rdata_rrsig_t sig;
-	int64_t when;
-	isc_result_t result;
+	bool seen = false;
+	int64_t when = UINT32_MAX;
 
 	REQUIRE(DNS_RDATASET_VALID(rdataset));
 
-	result = dns_rdataset_first(rdataset);
-	INSIST(result == ISC_R_SUCCESS);
-	dns_rdataset_current(rdataset, &rdata);
-	(void)dns_rdata_tostruct(&rdata, &sig, NULL);
-	if ((rdata.flags & DNS_RDATA_OFFLINE) != 0) {
-		when = 0;
-	} else {
-		when = dns_time64_from32(sig.timeexpire);
-	}
-	dns_rdata_reset(&rdata);
-
-	result = dns_rdataset_next(rdataset);
-	while (result == ISC_R_SUCCESS) {
+	DNS_RDATASET_FOREACH(rdataset) {
+		dns_rdata_t rdata = DNS_RDATA_INIT;
 		dns_rdataset_current(rdataset, &rdata);
+
+		dns_rdata_rrsig_t sig;
 		(void)dns_rdata_tostruct(&rdata, &sig, NULL);
 		if ((rdata.flags & DNS_RDATA_OFFLINE) != 0) {
-			goto next_rr;
+			continue;
+		} else {
+			seen = true;
+			int sigresign = dns_time64_from32(sig.timeexpire);
+			when = ISC_MIN(when, sigresign);
 		}
-		if (when == 0 || dns_time64_from32(sig.timeexpire) < when) {
-			when = dns_time64_from32(sig.timeexpire);
-		}
-	next_rr:
-		dns_rdata_reset(&rdata);
-		result = dns_rdataset_next(rdataset);
 	}
-	INSIST(result == ISC_R_NOMORE);
+
+	when = seen ? when : 0;
+
 	return (isc_stdtime_t)when;
 }
