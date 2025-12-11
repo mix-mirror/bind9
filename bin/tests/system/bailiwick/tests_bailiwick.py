2562243ac0f98f08330ef1eb45198788df6140a0
Michał Kępień <michal@isc.org>
2025-12-11 14:33:10 +0100

    Test that fake child delegation cannot overwrite parent's glue RR
    In short, the attacker tries to spoof at least one answer that has the
    following form:
    
        rcode NOERROR
        flags QR
        ;QUESTION
        trigger$RANDOM.victim. IN TXT
        ;ANSWER
        ;AUTHORITY
        trigger$RANDOM.victim. 3600 IN NS ns.victim.
        ;ADDITIONAL
        ns.victim. 3600 IN A 10.53.0.3
    
    This attack was originally reported as "test case 2".
    
    Co-authored-by: Michał Kępień <michal@isc.org>

--- bin/tests/system/bailiwick/tests_bailiwick.py
+++ bin/tests/system/bailiwick/tests_bailiwick.py
@@ -96,3 +96,16 @@ def test_bailiwick_unsolicited_authority(servers: Dict[str, NamedInstance]) -> N
     prime_cache(ns4)
     send_trigger_query(ns4, "trigger.victim.")
     check_domain_hijack(ns4)
+
+
+def test_bailiwick_parent_glue(servers: Dict[str, NamedInstance]) -> None:
+    set_spoofing_mode(ans1="none", ans2="parent-glue")
+
+    ns4 = servers["ns4"]
+    prime_cache(ns4)
+    send_trigger_query(ns4, "trigger.victim.")
+
+    isctest.log.info("Waiting 61 seconds for the ns.victim. ADB entry to expire")
+    time.sleep(61)
+
+    check_domain_hijack(ns4)
