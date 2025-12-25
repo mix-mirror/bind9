5922f99698dc6922f63577697da16b5382ac54a7
Alessio Podda <alessio@isc.org>
2025-12-26 00:13:27 +0100

    fixup! Do not modify the chain in find_closest_nsec

--- lib/dns/qpzone.c
+++ lib/dns/qpzone.c
@@ -2959,7 +2959,7 @@ previous_closest_nsec(dns_rdatatype_t type, qpz_search_t *search,
 
 		*nodep = NULL;
 		result = dns_qp_lookup(&search->qpr, &nsec_node->name,
-				       DNS_DBNAMESPACE_NORMAL, &search->iter,
+				       DNS_DBNAMESPACE_NORMAL, NULL,
 				       NULL, (void **)nodep, NULL);
 		if (result == ISC_R_SUCCESS) {
 			dns_name_copy(&nsec_node->name, name);
