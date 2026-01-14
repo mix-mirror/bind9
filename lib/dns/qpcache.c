34e7f3eb0cdde8a794c1da8678edfe17a68a9c43
Ondřej Surý <ondrej@isc.org>
2026-01-14 16:35:27 +0100

    Remove the heap memory context from QPcache
    The heaps have been removed, so the separate heap memory context
    (hmctx) is no longer needed.  Remove it from both dns_cache and
    dns_qpcache, along with the HeapMemInUse statistics.

--- lib/dns/qpcache.c
+++ lib/dns/qpcache.c
@@ -216,8 +216,6 @@ struct qpcache {
 	/* Locked by tree_lock. */
 	dns_qp_t *tree;
 
-	isc_mem_t *hmctx; /* Memory context for the heaps */
-
 	size_t buckets_count;
 	qpcache_bucket_t buckets[]; /* attribute((counted_by(buckets_count))) */
 };
@@ -2281,7 +2279,6 @@ qpcache__destroy(qpcache_t *qpdb) {
 	isc_rwlock_destroy(&qpdb->lock);
 	qpdb->common.magic = 0;
 	qpdb->common.impmagic = 0;
-	isc_mem_detach(&qpdb->hmctx);
 
 	isc_mem_putanddetach(&qpdb->common.mctx, qpdb,
 			     sizeof(*qpdb) + qpdb->buckets_count *
@@ -3171,7 +3168,6 @@ dns__qpcache_create(isc_mem_t *mctx, const dns_name_t *origin,
 		    unsigned int argc, char *argv[],
 		    void *driverarg ISC_ATTR_UNUSED, dns_db_t **dbp) {
 	qpcache_t *qpdb = NULL;
-	isc_mem_t *hmctx = mctx;
 	isc_loop_t *loop = isc_loop();
 	int i;
 	size_t nloops = isc_loopmgr_nloops();
@@ -3179,6 +3175,8 @@ dns__qpcache_create(isc_mem_t *mctx, const dns_name_t *origin,
 	/* This database implementation only supports cache semantics */
 	REQUIRE(type == dns_dbtype_cache);
 	REQUIRE(loop != NULL);
+	REQUIRE(argc == 0);
+	REQUIRE(argv == NULL);
 
 	qpdb = isc_mem_get(mctx,
 			   sizeof(*qpdb) + nloops * sizeof(qpdb->buckets[0]));
@@ -3191,13 +3189,6 @@ dns__qpcache_create(isc_mem_t *mctx, const dns_name_t *origin,
 		.references = 1,
 		.buckets_count = nloops,
 	};
-
-	/*
-	 * If argv[0] exists, it points to a memory context to use for heap
-	 */
-	if (argc != 0) {
-		hmctx = (isc_mem_t *)argv[0];
-	}
 
 	isc_rwlock_init(&qpdb->lock);
 	TREE_INITLOCK(&qpdb->tree_lock);
@@ -3219,7 +3210,6 @@ dns__qpcache_create(isc_mem_t *mctx, const dns_name_t *origin,
 	 * mctx won't disappear out from under us.
 	 */
 	isc_mem_attach(mctx, &qpdb->common.mctx);
-	isc_mem_attach(hmctx, &qpdb->hmctx);
 
 	/*
 	 * Make a copy of the origin name.
