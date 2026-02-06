0a83de4f7c80a1ed9709cc76592e826282241ca0
Mark Andrews <marka@isc.org>
2026-02-06 12:52:55 +1100

    Always record the request time with responses
    The description in the protobuf specifiation is not a list request
    types to process but is a list of examples to qualify the description
    of whether the time is when it is received or sent.

--- lib/dns/dnstap.c
+++ lib/dns/dnstap.c
@@ -778,17 +778,6 @@ dns_dt_send(dns_view_t *view, dns_dtmsgtype_t msgtype, isc_sockaddr_t *qaddr,
 		dm.m.has_response_time_sec = 1;
 		dm.m.response_time_nsec = isc_time_nanoseconds(t);
 		dm.m.has_response_time_nsec = 1;
-
-		/*
-		 * Types RR, FR, CR, and AR can fall through and get the query
-		 * time set as well. Any other response type, break.
-		 */
-		if (msgtype != DNS_DTTYPE_RR && msgtype != DNS_DTTYPE_FR &&
-		    msgtype != DNS_DTTYPE_CR && msgtype != DNS_DTTYPE_AR)
-		{
-			break;
-		}
-
 		FALLTHROUGH;
 	case DNS_DTTYPE_AQ:
 	case DNS_DTTYPE_CQ:
