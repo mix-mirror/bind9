26ec5ac5d1b18675234e98c78ab2c612c6f659c2
Evan Hunt <each@isc.org>
2026-02-23 16:27:52 -0800

    make the RD flag optional in isctest.query
    it is now possible to use isctest.query(..., rd=False) to send
    non-recursive queries.

--- bin/tests/system/isctest/query.py
+++ bin/tests/system/isctest/query.py
@@ -111,6 +111,7 @@ def create(
     qtype,
     qclass=dns.rdataclass.IN,
     dnssec: bool = True,
+    rd: bool = True,
     cd: bool = False,
     ad: bool = True,
 ) -> dns.message.Message:
@@ -118,7 +119,9 @@ def create(
     msg = dns.message.make_query(
         qname, qtype, qclass, use_edns=True, want_dnssec=dnssec
     )
-    msg.flags = dns.flags.RD
+    msg.flags = 0
+    if rd:
+        msg.flags = dns.flags.RD
     if ad:
         msg.flags |= dns.flags.AD
     if cd:
