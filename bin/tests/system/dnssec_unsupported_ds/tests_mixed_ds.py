2ceb17e649d527b6c9fd95c5dee30860f1007b99
Nicki Křížek <nicki@isc.org>
2026-03-20 13:05:46 +0100

    Refactor dnssec_unsupported_ds test
    - Replace sign.sh scripts with python code in bootstrap().
    - Replace the zone files with a common template.
    - Extend the test case to check for an expected log message, because
      SERVFAIL alone can indicate other failures.
    - Unify the named.conf files.

--- bin/tests/system/dnssec_unsupported_ds/tests_mixed_ds.py
+++ bin/tests/system/dnssec_unsupported_ds/tests_mixed_ds.py
@@ -9,9 +9,15 @@
 # See the COPYRIGHT file distributed with this work for additional
 # information regarding copyright ownership.
 
+from re import compile as Re
+
+import dns
+import os
 import pytest
 
 import isctest
+from isctest.vars.algorithms import Algorithm
+from isctest.template import Zone, Nameserver
 
 pytestmark = pytest.mark.extra_artifacts(
     [
@@ -20,17 +26,50 @@ pytestmark = pytest.mark.extra_artifacts(
         "ns*/keys/*.key",
         "ns*/keys/*.private",
         "ns*/trusted.conf",
-        "ns*/zones/*.db",
+        "ns*/zones/*.db.in",
         "ns*/zones/*.db.signed",
     ]
 )
 
 
+def modify_dsset():
+    with open("ns3/dsset-child.example.", encoding="utf-8") as dsset_file:
+        ds_orig = dsset_file.readline()
+
+    alg = Algorithm.default().number
+    alg_re = Re(rf"\s+{alg}\s+")
+    ds_unsupported = alg_re.sub(" 12 ", ds_orig)
+
+    digest_re = Re(rf"\s+{alg}\s+2\s+.*")
+    ds_bogus = digest_re.sub(f" {alg} 2 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", ds_orig)
+
+    with open("ns3/dsset-child.example.", "w", encoding="utf-8") as dsset_file:
+        dsset_file.writelines([ds_unsupported, ds_bogus])
+
+
+def bootstrap():
+    child = Zone("child.example", "child.example.db.signed", Nameserver("ns3", "10.53.0.3"))
+    isctest.setup.configure_signed_zone(child, [], template="template.db.j2.manual")
+
+    isctest.log.info("child.example: modify DS set to have unsupported and bogus DS records")
+    modify_dsset()
+
+    example = Zone("example", "example.db.signed", Nameserver("ns2", "10.53.0.2"))
+    isctest.setup.configure_signed_zone(example, [child], template="template.db.j2.manual")
+
+    ta = isctest.setup.configure_signed_root([example])
+    return {
+        "trust_anchors": [ta],
+    }
+
+
 def test_mixed_ds(ns9):
     msg = isctest.query.create("child.example.", "DNSKEY")
-    res = isctest.query.tcp(msg, ns9.ip)
+    with ns9.watch_log_from_here() as watcher:
+        res = isctest.query.tcp(msg, ns9.ip)
+        watcher.wait_for_line("child.example/DNSKEY: insecurity proof failed")
     isctest.check.servfail(res)
 
-    msg = isctest.query.create("child.example.", "A")
+    msg = isctest.query.create("a.child.example.", "A")
     res = isctest.query.tcp(msg, ns9.ip)
     isctest.check.servfail(res)
