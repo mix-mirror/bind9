b705135289acffd714b43d5fe42424b60a100904
Ondřej Surý <ondrej@sury.org>
2026-04-01 20:59:08 +0200

    Remove stale Net::DNS prereq checks from system tests
    These tests no longer use Perl ans servers (they were converted to
    Python AsyncDnsServer) but still had prereq.sh checks requiring the
    Perl Net::DNS module, preventing the tests from running on systems
    without it.

--- bin/tests/system/xfer/prereq.sh
+++ bin/tests/system/xfer/prereq.sh
@@ -11,20 +11,4 @@
 # See the COPYRIGHT file distributed with this work for additional
 # information regarding copyright ownership.
 
-. ../conf.sh
-
-# macOS ships with Net::DNS 0.74 which does not work with
-# HMAC-SHA256, despite the workarounds in ans.pl
-
-if ${PERL} -MNet::DNS -e 'exit ($Net::DNS::VERSION >= 1.0)'; then
-  version=$(${PERL} -MNet::DNS -e 'print $Net::DNS::VERSION')
-  echo_i "perl Net::DNS $version is too old - skipping xfer test"
-  exit 1
-fi
-
-if ! ${PERL} -MDigest::HMAC -e ''; then
-  echo_i "perl Digest::HMAC module is required"
-  exit 1
-fi
-
 exit 0
