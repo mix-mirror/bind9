77825492c1404c503df9c23feac05a76b46337bc
Colin Vidal <colin@isc.org>
2026-04-30 13:32:29 +0100

    Remove zonecut lookup in `query_delegation_recurse()`
    The parent-centric changes added an explicit `dns_view_bestzonecut()`
    call inside `query_delegation_recurse()` to ensure recursion used a
    zonecut from the delegdb rather than potentially incorrect data from the
    main cache (called in case it would have a cached answer).
    
    This is no longer necessary because `qpcache` no longer performs deepest
    zonecut selection (as part of further the parent-centric changes), so if
    there is no match for the QNAME, `qctx->rdataset` won't be attached, and
    the resolver would find by itself the best zonecut form the delegation
    database.
    
    However, there might be cases where `qctx->rdataset` is attached when
    resuming from recursion. As this came from the resolver, it is safe to
    keep (thus it is converted into a delegset then passed back to the
    resolver).
    
    This change also fixes the performance slowdown introduced with
    parent-centric changes.

--- lib/ns/query.c
+++ lib/ns/query.c
@@ -8658,26 +8658,16 @@ query_delegation_recurse(query_ctx_t *qctx) {
 		 * Any other recursion.
 		 */
 		dns_delegset_t *delegset = NULL;
-		dns_fixedname_t ffname;
-		dns_name_t *fname = dns_fixedname_initname(&ffname);
-		isc_result_t tresult;
+		dns_name_t *fname = NULL;
 
-		tresult = dns_view_bestzonecut(qctx->view, qname, fname, NULL,
-					       qctx->client->inner.now, 0, true,
-					       true, &delegset);
-		if (tresult != ISC_R_SUCCESS) {
+		if (dns_rdataset_isassociated(qctx->rdataset)) {
 			dns_delegset_fromnsrdataset(qctx->client->manager->mctx,
 						    qctx->rdataset, &delegset);
 			fname = qctx->fname;
 		}
 
-		if (delegset == NULL) {
-			result = ISC_R_NOTFOUND;
-		} else {
-			result = ns_query_recurse(qctx->client, qctx->qtype,
-						  qname, fname, delegset,
-						  qctx->resuming);
-		}
+		result = ns_query_recurse(qctx->client, qctx->qtype, qname,
+					  fname, delegset, qctx->resuming);
 
 		if (delegset != NULL) {
 			dns_delegset_detach(&delegset);
