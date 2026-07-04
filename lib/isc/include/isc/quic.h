d907e05c3beded23e46ae2f7ef8091b1e8521134
Aydın Mercan <aydin@isc.org>
2026-07-04 11:11:21 +0300

    add routing for quic
    QUIC is a protocol that is agnostic of the source address/port and uses
    connection IDs and stateless reset tokens to distinguish sessions.
    
    The thread-safe `isc_quic_router_t` structure keeps track of this
    information and also has functionality for inspecting packets to find
    the appropriate action alongside the associated connection if it exists.

---
+++ lib/isc/include/isc/quic.h
@@ -0,0 +1,230 @@
+/*
+ * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See the COPYRIGHT file distributed with this work for additional
+ * information regarding copyright ownership.
+ */
+
+#pragma once
+
+#include <isc/refcount.h>
+#include <isc/region.h>
+#include <isc/tid.h>
+#include <isc/types.h>
+
+/*! \file isc/quic.h */
+
+/**
+ * \brief
+ * Minimum allowed length of a CID.
+ *
+ * Specified in RFC9000, Section 17.2.
+ */
+#define ISC_QUIC_CID_MIN_LENGTH 1
+
+/**
+ * \brief
+ * Maximum allowed length of a CID.
+ *
+ * Specified in RFC9000, Section 17.2.
+ */
+#define ISC_QUIC_CID_MAX_LENGTH 20
+
+/**
+ * \brief
+ * Length of the stateless reset token.
+ *
+ * Specified in RFC9000, Section 10.3.
+ */
+#define ISC_QUIC_STATELESS_TOKEN_LENGTH 16
+
+/**
+ * \brief
+ * Thread-safe data structure for associating CID's and stateless reset tokens
+ * to QUIC connections.
+ */
+typedef struct isc_quic_router isc_quic_router_t;
+
+typedef enum isc_quic_version {
+	/** Invalid QUIC version */
+	ISC_QUIC_VERSION_INVALID = 0,
+
+	/** Unknown QUIC version */
+	ISC_QUIC_VERSION_UNKNOWN = 1,
+
+	/** RFC9000 */
+	ISC_QUIC_VERSION_V1 = 2,
+
+	/** RFC9369 */
+	ISC_QUIC_VERSION_V2 = 3,
+
+	ISC_QUIC_VERSION__MAX = 4,
+} isc_quic_version_t;
+
+void
+isc_quic_router_create(isc_mem_t *mctx, isc_quic_router_t **routerp);
+/**<
+ * \brief
+ * Create a new QUIC CID router.
+ *
+ * \par Requires:
+ * \li `mctx` is a valid memory context.
+ * \li `routerp != NULL` and `*routerp == NULL`
+ */
+
+ISC_REFCOUNT_DECL(isc_quic_router);
+
+isc_result_t
+isc_quic_router_add_cid(isc_quic_router_t *router, isc_constregion_t cid,
+			void *conn);
+/**<
+ * \brief
+ * Associate a connection with the given CID.
+ *
+ * The CID will be copied and thus the parameter free from any lifetime
+ * requirements.
+ *
+ * The current thread will be used for the TID of the connection.
+ *
+ * \par Requires:
+ * \li `router` is a valid router.
+ * \li `cid.base != NULL` and `cid.length` ∈ [1, 20]
+ * \li `conn != NULL`
+ *
+ * \retval ISC_R_SUCCESS on success
+ * \retval ISC_R_EXISTS if a connection exists for the given CID
+ */
+
+isc_result_t
+isc_quic_router_get_cid(isc_quic_router_t *router, isc_constregion_t cid,
+			isc_tid_t *tidp, void **connp);
+/**<
+ * \brief
+ * Get the connection associated with a given CID and its thread if `tidp` is
+ * not NULL.
+ *
+ * \par Requires:
+ * \li `router` is a valid router.
+ * \li `cid.base != NULL` and `cid.length` ∈ [1, 20]
+ *
+ * \retval ISC_R_SUCCESS on success
+ * \retval ISC_R_NOTFOUND if no connection exists for the given CID.
+ */
+
+isc_result_t
+isc_quic_router_del_cid(isc_quic_router_t *router, isc_constregion_t cid);
+/**<
+ * \brief
+ * Remove the connection associated with the given CID.
+ *
+ * \par Requires:
+ * \li `router` is a valid router.
+ * \li `cid.base != NULL` and `cid.length` ∈ [1, 20]
+ *
+ * \retval ISC_R_SUCCESS on success
+ * \retval ISC_R_NOTFOUND if no connection exists for the given CID.
+ */
+
+isc_result_t
+isc_quic_router_add_stateless_reset(
+	isc_quic_router_t *router,
+	const uint8_t	   token[restrict ISC_QUIC_STATELESS_TOKEN_LENGTH],
+	void		  *conn);
+/**<
+ * \brief
+ * Associate a connection with the given stateless reset token.
+ *
+ * The current thread will be used for the TID of the connection.
+ *
+ * The token will be copied and thus the parameter free from any lifetime
+ * requirements.
+ *
+ * \par Requires:
+ * \li `router` is a valid router.
+ * \li `token != NULL`
+ * \li `conn != NULL`
+ *
+ * \retval ISC_R_SUCCESS on success
+ * \retval ISC_R_EXISTS if a connection is already associated with the given
+ * token.
+ */
+
+isc_result_t
+isc_quic_router_get_stateless_reset(
+	isc_quic_router_t *router,
+	const uint8_t token[const restrict ISC_QUIC_STATELESS_TOKEN_LENGTH],
+	isc_tid_t *tidp, void **connp);
+/**<
+ * \brief
+ * Get the connection associated with the given stateless reset token and its
+ * thread if `tidp` is not NULL.
+ *
+ * \par Requires:
+ * \li `router` is a valid router.
+ * \li `token` != NULL
+ *
+ * \retval ISC_R_SUCCESS on success
+ * \retval ISC_R_NOTFOUND if no connection exists for the given token.
+ */
+
+isc_result_t
+isc_quic_router_del_stateless_reset(
+	isc_quic_router_t *router,
+	const uint8_t token[const restrict ISC_QUIC_STATELESS_TOKEN_LENGTH]);
+/**<
+ * \brief
+ * Remove the connection associated with the given stateless reset token.
+ *
+ * \par Requires:
+ * \li `router` is a valid router.
+ * \li `token != NULL`
+ *
+ * \retval ISC_R_SUCCESS on success
+ * \retval ISC_R_NOTFOUND if no connection exists for the given token.
+ */
+
+isc_result_t
+isc_quic_router_handle_packet(isc_quic_router_t	 *router,
+			      isc_constregion_t	  packet,
+			      isc_quic_version_t *versionp,
+			      isc_constregion_t	 *dcidp,
+			      isc_constregion_t *scidp, isc_tid_t *tidp,
+			      void **connp);
+/**<
+ * \brief
+ * Extracts the associated value and relevant information of a given QUIC
+ * packet.
+ *
+ * \par Requires:
+ * \li `router` is a valid router.
+ * \li `packet.base != NULL` and `packet.length != 0`
+ * \li `dcidp != NULL`
+ * \li `scidp != NULL`
+ * \li `connp != NULL` and `*connp == NULL`
+ *
+ * It checks for the following
+ * \li Check if the packet header is valid. If so, extract relevant information.
+ * \li If the DCID is associated with a value, return it.
+ * \li Otherwise, check if the packet version is unknown. If so, also check if
+ * it has a stateless reset token.
+ * \li Check if it can be accepted as a handshake.
+ *
+ * \warning
+ * The router will **NOT** automatically remove the CID on stateless reset.
+ *
+ * \retval ISC_R_SUCCESS when an associated connection for the packet is found
+ * \retval ISC_R_UNSET when a stateless reset packet for a session has been
+ * found.
+ * \retval ISC_R_NOTFOUND when the packet should be accepted as the handshake of
+ * a new connection.
+ * \retval ISC_R_INVALIDPROTO on packet misformat
+ * \retval ISC_R_UNEXPECTED when the packet isn't associated with a connection
+ * yet cannot be accepted as a handshake.
+ * \retval ISC_R_IGNORE when the packet should be ignored silently.
+ */
---
+++ tests/isc/quic_test.c
@@ -0,0 +1,261 @@
+/*
+ * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See the COPYRIGHT file distributed with this work for additional
+ * information regarding copyright ownership.
+ */
+
+#include <inttypes.h>
+#include <sched.h>  /* IWYU pragma: keep */
+#include <setjmp.h> /* IWYU pragma: keep */
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h> /* IWYU pragma: keep */
+#include <unistd.h>
+
+#define UNIT_TESTING
+#include <cmocka.h>
+
+#include <isc/lib.h>
+#include <isc/quic.h>
+#include <isc/random.h>
+
+#include <tests/isc.h>
+
+constexpr uint8_t client_dcid[] = {
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+};
+
+constexpr uint8_t unknown_cid[] = {
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+};
+
+/* Adapted from https://quic.xargs.org/ */
+constexpr uint8_t initial_frame_v1[1200] = {
+	/*
+	 * Long Header Packet Byte (RFC9000, Section 17.2):
+	 * 11000000
+	 * ││├┘├┘├┘
+	 * │││ │ └── Packet number field length (1-byte)
+	 * │││ └──── Reserved Bits (RFC9000, Section 17.2.2)
+	 * ││└────── Long Packet Type (RFC9000, Section 17.2, Table 5)
+	 * │└─────── Fixed Bit
+	 * └──────── Header Form
+	 */
+	0xc0,
+	/* QUIC version v1 */
+	0x00, 0x00, 0x00, 0x01,
+	/* 8-bytes of DCID to follow */
+	0x08,
+	/* DCID */
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+	/* 5-bytes of SCID to follow */
+	0x05,
+	/* SCID */
+	0xF0, 0xF1, 0xF2, 0xF3, 0xF4,
+	/* 0-bytes of token to follow */
+	0x00,
+	/* 259-bytes of packet to follow */
+	0x41, 0x03,
+	/* Packet number */
+	0x00
+	/* The rest is zero-padded packet information that won't be inspected */
+};
+
+constexpr uint8_t initial_frame_reserved_version[1200] = {
+	/*
+	 * Long Header Packet Byte (RFC9000, Section 17.2):
+	 * 11000000
+	 * ││├┘├┘├┘
+	 * │││ │ └── Packet number field length (1-byte)
+	 * │││ └──── Reserved Bits (RFC9000, Section 17.2.2)
+	 * ││└────── Long Packet Type (RFC9000, Section 17.2, Table 5)
+	 * │└─────── Fixed Bit
+	 * └──────── Header Form
+	 */
+	0xc0,
+	/* QUIC reserved version */
+	0x0A, 0x0A, 0x0A, 0x0A,
+	/* 8-bytes of DCID to follow */
+	0x08,
+	/* DCID */
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+	/* 5-bytes of SCID to follow */
+	0x05,
+	/* SCID */
+	0xF0, 0xF1, 0xF2, 0xF3, 0xF4,
+	/* 0-bytes of token to follow */
+	0x00,
+	/* 259-bytes of packet to follow */
+	0x41, 0x03,
+	/* Packet number */
+	0x00
+	/* The rest is zero-padded packet information that won't be inspected */
+};
+
+constexpr uint8_t stateless_reset_token[ISC_QUIC_STATELESS_TOKEN_LENGTH] = {
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+	0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0xE,  0x0F,
+};
+
+constexpr uint8_t stateless_reset_packet[] = {
+	0x40, 0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0x00, 0x01, 0x02, 0x03,
+	0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0xE,  0x0F,
+};
+
+ISC_RUN_TEST_IMPL(isc_quic_router_cid) {
+	isc_quic_router_t *router = NULL;
+	isc_constregion_t cid;
+	isc_result_t result;
+	void *value = NULL;
+
+	isc_quic_router_create(isc_g_mctx, &router);
+
+	cid = (isc_constregion_t){ client_dcid, sizeof(client_dcid) };
+	result = isc_quic_router_add_cid(router, cid, router);
+	assert_int_equal(result, ISC_R_SUCCESS);
+
+	cid = (isc_constregion_t){ client_dcid, sizeof(client_dcid) };
+	result = isc_quic_router_add_cid(router, cid, router);
+	assert_int_equal(result, ISC_R_EXISTS);
+
+	cid = (isc_constregion_t){ client_dcid, sizeof(client_dcid) };
+	result = isc_quic_router_get_cid(router, cid, NULL, &value);
+	assert_int_equal(result, ISC_R_SUCCESS);
+	assert_ptr_equal(value, router);
+
+	value = NULL;
+	cid = (isc_constregion_t){ unknown_cid, sizeof(unknown_cid) };
+	result = isc_quic_router_get_cid(router, cid, NULL, &value);
+	assert_int_equal(result, ISC_R_NOTFOUND);
+
+	cid = (isc_constregion_t){ unknown_cid, sizeof(unknown_cid) };
+	result = isc_quic_router_add_cid(router, cid, router);
+	assert_int_equal(result, ISC_R_SUCCESS);
+
+	isc_quic_router_detach(&router);
+}
+
+ISC_RUN_TEST_IMPL(isc_quic_router_stateless_reset) {
+	uint8_t token[ISC_QUIC_STATELESS_TOKEN_LENGTH];
+	isc_quic_router_t *router = NULL;
+	isc_result_t result;
+	void *value = NULL;
+
+	isc_quic_router_create(isc_g_mctx, &router);
+
+	isc_random_buf(token, sizeof(token));
+
+	result = isc_quic_router_get_stateless_reset(router, token, NULL,
+						     &value);
+	assert_int_equal(result, ISC_R_NOTFOUND);
+
+	result = isc_quic_router_add_stateless_reset(router, token, router);
+	assert_int_equal(result, ISC_R_SUCCESS);
+
+	result = isc_quic_router_add_stateless_reset(router, token, router);
+	assert_int_equal(result, ISC_R_EXISTS);
+
+	result = isc_quic_router_get_stateless_reset(router, token, NULL,
+						     &value);
+	assert_int_equal(result, ISC_R_SUCCESS);
+	assert_ptr_equal(value, router);
+
+	result = isc_quic_router_del_stateless_reset(router, token);
+	assert_int_equal(result, ISC_R_SUCCESS);
+
+	result = isc_quic_router_get_stateless_reset(router, token, NULL,
+						     &value);
+	assert_int_equal(result, ISC_R_NOTFOUND);
+
+	result = isc_quic_router_del_stateless_reset(router, token);
+	assert_int_equal(result, ISC_R_NOTFOUND);
+
+	isc_quic_router_detach(&router);
+}
+
+ISC_RUN_TEST_IMPL(isc_quic_router_packet) {
+	isc_quic_router_t *router = NULL;
+	isc_quic_version_t version;
+	isc_constregion_t dcid, scid;
+	isc_result_t result;
+	void *value = NULL;
+
+	isc_quic_router_create(isc_g_mctx, &router);
+
+	/* Initial CRYPTO frames MUST be 1200 bytes */
+	result = isc_quic_router_handle_packet(
+		router, (isc_constregion_t){ initial_frame_v1, 1199 }, &version,
+		&dcid, &scid, NULL, &value);
+	assert_int_equal(result, ISC_R_IGNORE);
+	assert_int_equal(version, ISC_QUIC_VERSION_V1);
+
+	/* Use reserved version 0x0A0A0A0A */
+	result = isc_quic_router_handle_packet(
+		router,
+		(isc_constregion_t){ initial_frame_reserved_version, 1200 },
+		&version, &dcid, &scid, NULL, &value);
+	assert_int_equal(result, ISC_R_FAILURE);
+	assert_int_equal(version, ISC_QUIC_VERSION_UNKNOWN);
+
+	result = isc_quic_router_handle_packet(
+		router, (isc_constregion_t){ initial_frame_v1, 1200 }, &version,
+		&dcid, &scid, NULL, &value);
+	assert_int_equal(result, ISC_R_NOTFOUND);
+	assert_int_equal(version, ISC_QUIC_VERSION_V1);
+	assert_int_equal(dcid.length, sizeof(client_dcid));
+	assert_memory_equal(dcid.base, client_dcid, dcid.length);
+
+	result = isc_quic_router_add_cid(router, dcid, router);
+	assert_int_equal(result, ISC_R_SUCCESS);
+
+	result = isc_quic_router_handle_packet(
+		router, (isc_constregion_t){ initial_frame_v1, 1200 }, &version,
+		&dcid, &scid, NULL, &value);
+	assert_int_equal(result, ISC_R_SUCCESS);
+	assert_int_equal(version, ISC_QUIC_VERSION_V1);
+	assert_ptr_equal(value, router);
+
+	value = NULL;
+	result = isc_quic_router_handle_packet(
+		router, (isc_constregion_t){ initial_frame_v1, 500 }, &version,
+		&dcid, &scid, NULL, &value);
+	assert_int_equal(result, ISC_R_SUCCESS);
+	assert_ptr_equal(value, router);
+
+	result = isc_quic_router_del_cid(router, dcid);
+	assert_int_equal(result, ISC_R_SUCCESS);
+
+	value = NULL;
+	result = isc_quic_router_handle_packet(
+		router, (isc_constregion_t){ initial_frame_v1, 1200 }, &version,
+		&dcid, &scid, NULL, &value);
+	assert_int_equal(result, ISC_R_NOTFOUND);
+
+	result = isc_quic_router_add_stateless_reset(
+		router, stateless_reset_token, router);
+	assert_int_equal(result, ISC_R_SUCCESS);
+
+	result = isc_quic_router_handle_packet(
+		router,
+		(isc_constregion_t){ stateless_reset_packet,
+				     sizeof(stateless_reset_packet) },
+		NULL, &dcid, &scid, NULL, &value);
+	assert_int_equal(result, ISC_R_UNSET);
+
+	isc_quic_router_detach(&router);
+}
+
+ISC_TEST_LIST_START
+ISC_TEST_ENTRY(isc_quic_router_cid)
+ISC_TEST_ENTRY(isc_quic_router_stateless_reset)
+ISC_TEST_ENTRY(isc_quic_router_packet)
+ISC_TEST_LIST_END
+
+ISC_TEST_MAIN
