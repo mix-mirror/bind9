11d06c654d0c535bb3845a3d27b32dc602d1ef5d
Aydın Mercan <aydin@isc.org>
2026-08-27 17:40:19 +0300

    fixup! initial definition of quic connection

--- lib/isc/include/isc/types.h
+++ lib/isc/include/isc/types.h
@@ -79,12 +79,10 @@ typedef struct isc_timer      isc_timer_t;	/*%< Timer */
 typedef struct isc_work	      isc_work_t;	/*%< Work offloaded to an
 						 *   external thread */
 
-#ifdef HAVE_LIBNGTCP2
 typedef struct isc_quic_conn		 isc_quic_conn_t;
 typedef struct isc_quic_conn_options	 isc_quic_conn_options_t;
 typedef struct isc_quic_conn_callbacks	 isc_quic_conn_callbacks_t;
 typedef struct isc_quic_stream_data_info isc_quic_stream_data_info_t;
-#endif /* HAVE_LIBNGTCP2 */
 
 #if HAVE_LIBNGHTTP2
 typedef struct isc_nm_http_endpoints isc_nm_http_endpoints_t;
