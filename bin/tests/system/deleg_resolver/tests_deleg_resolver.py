a0096e2b8a68f0912f6486e1256a5c0a33fb40dd
Colin Vidal <colin@isc.org>
2026-09-01 16:10:22 +0200

    Experimental DELEG/resolver without DNSSEC
    Add experimental support of DELEG on the resolver. DNSSEC validation is
    not yet supported.
    
    TODO: split this commit.

---
+++ bin/tests/system/deleg_resolver/tests_deleg_resolver.py
@@ -0,0 +1,40 @@
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0.  If a copy of the MPL was not distributed with this
+# file, you can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+
+import pytest
+from dns.rcode import NOERROR, SERVFAIL
+
+import isctest
+
+
+@pytest.mark.parametrize(
+    "qname, nsname, rcode",
+    [
+        ("a.delegonly", "ns10", NOERROR),
+        ("a.mixed", "ns10", NOERROR),
+        ("a.delegname", "ns10", NOERROR),
+        ("a.delegips", "ns10", NOERROR),
+        ("a.delegips4", "ns10", NOERROR),
+        ("a.delegmultiple", "ns10", NOERROR),
+        ("a.delegmandatory", "ns10", NOERROR),
+        ("a.delegunsupportedman", "ns2", NOERROR),
+        ("a.delegunsupportedman", "ns10", SERVFAIL),
+        ("a.delegunsupportedman2", "ns10", NOERROR),
+        ("a.delegparam", "ns10", NOERROR),
+        ("b.delegparam", "ns10", NOERROR),
+        ("a.delegparam2", "ns10", SERVFAIL),
+        ("a.delegparam3", "ns10", NOERROR),
+    ],
+)
+def test_deleg_resolver(qname, nsname, rcode, servers):
+    msg = isctest.query.create(qname, "A")
+    res = isctest.query.udp(msg, servers[nsname].ip)
+    isctest.check.rcode(res, rcode)
