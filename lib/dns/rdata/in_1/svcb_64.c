7c9298825ee3a2ffb11208a78c28b211fcb2c212
Alessio Podda <alessio@isc.org>
2026-09-13 15:41:01 +0200

    Revert "reject NUL bytes in SVCB port and base64 SvcParam values"
    This reverts commit 71627f0ea497846126de3b0981b7e877f3c5870c.

--- lib/dns/rdata/in_1/svcb_64.c
+++ lib/dns/rdata/in_1/svcb_64.c
@@ -278,9 +278,7 @@ svc_fromtext(isc_textregion_t *region, isc_buffer_t *target) {
 			break;
 		case sbpr_port:
 			if (region->length == 0 ||
-			    !isdigit((unsigned char)region->base[0]) ||
-			    /* Reject embedded NUL bytes. */
-			    memchr(region->base, 0, region->length) != NULL)
+			    !isdigit((unsigned char)region->base[0]))
 			{
 				return DNS_R_SYNTAX;
 			}
@@ -326,10 +324,6 @@ svc_fromtext(isc_textregion_t *region, isc_buffer_t *target) {
 			} while (e != NULL);
 			break;
 		case sbpr_base64:
-			/* Reject embedded NUL bytes. */
-			if (memchr(region->base, 0, region->length) != NULL) {
-				return DNS_R_SYNTAX;
-			}
 			RETERR(isc_base64_decodestring(region->base, target));
 			break;
 		case sbpr_empty:
