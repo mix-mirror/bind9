4db1516105a1f64b4e67d5471407c33a1daade3e
Aydın Mercan <aydin@isc.org>
2026-09-17 10:04:24 +0200

    fixup! add quic connection interface

--- lib/isc/quic/connection.c
+++ lib/isc/quic/connection.c
@@ -2461,11 +2461,16 @@ isc_quic_conn_server_create(
 	ngtcp2_conn_set_tls_native_handle(conn->inner, tls);
 	tls = NULL;
 
-	result = isc_quic_router_add_cid(router, initial_dcid, isc_tid(), conn);
 	/*
-	 * Probability is 2^{-160}, even this is actually not worth it the
-	 * slightest.
+	 * Probability is 2^{-160}, even these checks are actually not worth it
+	 * the slightest.
 	 */
+	result = isc_quic_router_add_cid(router, initial_dcid, isc_tid(), conn);
+	INSIST(result == ISC_R_SUCCESS);
+
+	result = isc_quic_router_add_cid(
+		router, (isc_constregion_t){ scid.data, scid.datalen },
+		isc_tid(), conn);
 	INSIST(result == ISC_R_SUCCESS);
 
 	*connp = MOVE_OWNERSHIP(conn);
