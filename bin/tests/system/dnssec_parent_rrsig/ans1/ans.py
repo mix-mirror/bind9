c61c9bd8e8fc45465f8b0e4f08cdb826edd84f35
Michał Kępień <michal@isc.org>
2026-09-22 12:23:24 +0200

    Adjust dnssec_parent_rrsig

--- bin/tests/system/dnssec_parent_rrsig/ans1/ans.py
+++ bin/tests/system/dnssec_parent_rrsig/ans1/ans.py
@@ -79,7 +79,7 @@ def rrset(owner: str, rdtype: dns.rdatatype.RdataType, *rdatas: str) -> dns.rrse
 
 
 def rrset_from_rdata(owner: str, rdata: dns.rdata.Rdata) -> dns.rrset.RRset:
-    return dns.rrset.from_rdata(name(owner), TTL, rdata)
+    return dns.rrset.from_rdata(name(owner), TTL if rdata.rdtype != dns.rdatatype.DNSKEY else 3600, rdata)
 
 
 def add_signed(
@@ -101,7 +101,7 @@ def soa_rrset(zone: str) -> dns.rrset.RRset:
     return rrset(
         zone,
         dns.rdatatype.SOA,
-        f"ns.{zone} hostmaster.{zone} 1 3600 600 86400 300",
+        f"ans1.{zone} . 1 20 20 1814400 3600",
     )
 
 
