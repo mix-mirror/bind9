be2f4849f1f9bf2e2af77a3630c0bbc6dc92659e
Michał Kępień <michal@isc.org>
2026-09-22 11:53:21 +0200

    ⏪ Revert "Check wildcard signer and NOQNAME signer match"
    This reverts commit 45c9bd26032c6bc93a1d04acc94f4db895b59b1b.

--- lib/dns/zoneverify.c
+++ lib/dns/zoneverify.c
@@ -186,7 +186,7 @@ goodsig(const vctx_t *vctx, dns_rdata_t *sigrdata, const dns_name_t *name,
 			continue;
 		}
 		result = dns_dnssec_verify(name, rdataset, dstkeys[key], false,
-					   vctx->mctx, sigrdata, NULL, NULL);
+					   vctx->mctx, sigrdata, NULL);
 		if (result == ISC_R_SUCCESS || result == DNS_R_FROMWILDCARD) {
 			return true;
 		}
